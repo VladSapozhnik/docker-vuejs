109b8558c3328e2e99a21c082c6c2ac0e3ac8776
Work <vladbars2@gmail.com>
2021-06-18 16:08:50 +0300

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="stack">
+    <div class="container">
+      <h1>Stack - обойма</h1>
+      <div class="stack__logo"></div>
+      <div class="stack__patron" >Патронов заряжено: {{patron}}</div>
+      <div class="stack__check">{{check}}</div>
+      <div class="stack__wrapper">
+        <div class="stack__wrapper-btn" @click="infoItem">Проверить магазин</div>
+        <div class="stack__wrapper-btn" @click="pushItem">Зарядить</div>
+        <div class="stack__wrapper-btn" @click="popItem">Нажать на курок</div>
+        <div class="stack__wrapper-btn" @click="swapItem">Сменить магазин</div>
+      </div>
+      <div class="stack__action">{{action}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      stack: [],
+      item: '',
+      patron: 0,
+      action: '',
+      check: '',
+      fullItem: [30, 29, 28, 27, 26],
+    }
+  },
+  methods: {
+    swapItem:function () {
+      this.stack = this.fullItem;
+      console.log(this.stack)
+      this.stack.pop();
+      this.fullItem++;
+      this.action = 'Зарядил';
+    },
+    pushItem:function () {
+        if (this.stack.length < 30) {
+          this.stack.push(this.item + this.patron);
+          this.patron++;
+          this.action = 'Зарядил';
+        } 
+    },
+
+    popItem:function () {
+      if (this.stack.length > 0) { 
+          this.stack.pop();
+          this.patron--;
+          this.action = 'Выстрел';
+      } else {
+        this.patron == 0; 
+        this.action = 'Выстрел невозможен магазин пуст!'; 
+      }
+    },
+
+    infoItem:function () {
+      if (this.stack.length == 0) {
+        this.check = 'Пуст.'
+      } else if (this.stack.length < 10) {
+        this.check = 'Одна четвертая.'
+      } else if (this.stack.length < 18) {
+        this.check = 'Пол магазина.'
+      } else if (this.stack.length < 25) {
+        this.check = 'Три четвертых.'
+      } else if (this.stack.length < 29) {
+        this.check = 'Почти полный.'
+      } else {
+        this.check = 'Полный.'
+      }
+    },
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  max-width: 920px;
+  margin: 0 auto;
+  border: 1px solid #000;
+  padding: 100px 0;
+}
+.stack {
+  min-height: 800px;
+  &__logo {
+    width: 100%;
+    height: 300px;
+    background-repeat: no-repeat;
+    background-image: url(/clip.jpg);
+    background-size: contain;
+    background-position: center;
+  }
+  &__patron {
+    font-size: 28px;
+    margin-bottom: 30px;
+  }
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 30px;
+    &-btn {
+      padding: 20px 40px;
+      color: #fff;
+      background-color: #000;
+      border-radius: 10px;
+      cursor: pointer;
+      & + & {
+        margin-left: 30px;
+      }
+    }
+  } 
+  &__check {
+    font-size: 28px;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  &__action {
+    font-size: 28px;
+    text-align: center;
+    color: rgb(151, 53, 53);
+  }
+}
+</style>

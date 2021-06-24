0733a3242d099829ef5f9ac64aaad2d65a833873
Work <vladbars2@gmail.com>
2021-06-24 15:27:35 +0300

    создал popup form

--- src/components/SearchAmmo.vue
+++ src/components/SearchAmmo.vue
@@ -3,9 +3,22 @@
     <div class="container">
       <div class="search__inner">
         <input class="search__inner-input" v-model="searchValue" type="text" @click="putSearch" placeholder="Какие патроны вы ищите?">
-        <div class="search__inner-title">Ammu-Nation</div>
+        <div class="search__inner-title" >Ammu-Nation</div>
         <div class="total">
-          <h3> Total : &euro;{{ammoPrice}} </h3>
+          <h3 class="search__total" @click="openPopup = true"> Total : &euro;{{ammoPrice}} </h3>
+          <div v-if="openPopup == true">
+            <div class="popup">
+               <h3>Ammu-Nation</h3>
+               <div class="popup__wrapper" >
+                  <input class="popup__wrapper-data" type="text" placeholder="Ваше имя"/>
+                  <input class="popup__wrapper-data" type="text" placeholder="Ваша фамилия">
+                  <input class="popup__wrapper-data" type="tel" placeholder="Ваше телефон">
+                  <input class="popup__wrapper-data" type="email" placeholder="Ваш email">
+                  <div @click="sendAmmo(ammoPrice)">Отправить</div>
+               </div>
+            </div>
+            <div class="popup-blackout" @click="openPopup = false"></div>
+          </div>
         </div>
       </div>
       <ul class="search__list" v-for="item in sortSearchItem" :key="item.id">
@@ -20,6 +33,7 @@
   </div>
 </template>
 <script>
+import { required, minLength } from 'vuelidate/lib/validators'
 export default {
   name: "SearchAmmo",
   data: function () {
@@ -28,8 +42,16 @@ export default {
           searchValue: '',
           cardSumm: [],
           num: 1,
-          ammoPrice: ''
+          ammoPrice: '0',
+          openPopup: false,
+          name: ''
       };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
   },
   computed: {
     sortSearchItem: function () {
@@ -45,6 +67,9 @@ export default {
             this.searchData = response.data;
         });
     },
+    sendAmmo: function(Price) {
+      console.log(Price)
+    },
     closeSearch: function() {
       this.searchData = []
     },
@@ -57,10 +82,10 @@ export default {
       this.ammoPrice =  Number(this.ammoPrice) + Number(add.price);
     },
     numProductTake: function(add) {
-      if (this.ammoPrice > 0) {
+      if (add.numProduct !== 0) {
         add.numProduct = Number(add.numProduct--) - this.num;
-        this.ammoPrice =  Number(this.ammoPrice) - Number(add.price);
-      } 
+        this.ammoPrice =  Number(this.ammoPrice) - Number(add.price); 
+      }
     }
   },
 };
@@ -69,6 +94,36 @@ export default {
 .search {
   position: relative;
   margin-bottom: 50px;
+    .popup {
+      max-width: 500px;
+      background-color: #fff;
+      position: fixed;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 5px;
+      padding: 50px 20px;
+      z-index: 10;
+      &-blackout {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.7);
+        z-index: 8;
+      }
+      &__wrapper {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        &-data {
+          width: calc(50% - 15px);
+          height: 30px;
+          margin-bottom: 20px;
+        }
+      }
+    }
     &__close {
       display: inline-block;
       font-size: 23px;
@@ -79,6 +134,12 @@ export default {
       background-color: #000;
       padding: 10px 20px;
       border-radius: 5px;
+    }
+    &__total {
+      border: 1px solid #000;
+      padding: 10px 20px;
+      border-radius: 5px;
+      cursor: pointer;
     }
     &__inner {
         display: flex;
